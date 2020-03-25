1c3cadeca5beb6eab10ff10c03092ce641a280c8
andres91co <carandgon91@hotmail.com>
2020-03-24 23:21:45 -0500

    ajuste

--- src/app/components/content/GRIFT/investigacion-institucional/investigacion-institucional.component.ts
+++ src/app/components/content/GRIFT/investigacion-institucional/investigacion-institucional.component.ts
@@ -150,7 +150,7 @@ export class InvestigacionInstitucionalComponent implements OnInit {
       ciudadForm: [''],
       paisForm: [''],
       departamentoForm:[''],
-
+      ciudadInternacionalForm: ['']
       
     });
 
